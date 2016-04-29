27ae0b0e405dc5811dd1b3615d1961f8663fd6a4
Res2013 <1033857564@qq.com>
2016-04-29 18:48:41 +0800

    modified by Res2013 in 2016-04-29 to add the RxAndroid into the ZXing demo

---
+++ Zxing_RxAndroid_Test/app/src/main/java/com/anonymous/myapplication/MainActivity.java
@@ -0,0 +1,121 @@
+package com.anonymous.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.client.android.decode.CaptureActivity;
+import com.google.zxing.client.android.encode.QRCodeEncoder;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.encoder.QRCode;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 0;
+
+    @Bind(R.id.qrcode_encode) Button mEncodeBtn;
+    @Bind(R.id.qrcode_decode) Button mDecodeBtn;
+    @Bind(R.id.qrcode_img) ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        // 生成二维码点击事件
+        mEncodeBtn.setOnClickListener(this::generateBitmap);
+        // 扫描二维码点击事件
+        mDecodeBtn.setOnClickListener(this::gotoCapture);
+        // 长按解析二维码事件
+        mImageView.setOnLongClickListener(this::decodeQrcode);
+    }
+
+    /**
+     * 生成二维码
+     */
+    public void generateBitmap(View v) {
+        try {
+            Bitmap mBitmap = QRCodeEncoder.encodeAsBitmap("https://github.com/Res2013/AndroidProgram.git", 600);
+            mImageView.setImageBitmap(mBitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void gotoCapture(View v) {
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    /**
+    * 解析二维码
+    */
+    public boolean decodeQrcode(View v) {/** 看起来变量v有点多余,为啥要用?是因为参数和返回值同Lambda表达式一样,可用方法引用形式(即用方法名) */
+        mImageView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(mImageView.getDrawingCache());
+        mImageView.setDrawingCacheEnabled(false);
+        decodeBitmap(bitmap);
+        return true;
+    }
+
+    /**
+     * 要解析的二维码图片
+     */
+    public final Map<DecodeHintType,Object> HINTS = new EnumMap<>(DecodeHintType.class);
+
+    public void decodeBitmap(Bitmap bitmap) {
+        Observable.just(bitmap)/** just:获取输入数据,直接分发,更加简洁,省略其他回调 */
+                .map(this::doInBackground)/** map:映射,对输入数据进行加工 */
+                .subscribeOn(Schedulers.newThread())/** 子线程中执行 */
+                .observeOn(AndroidSchedulers.mainThread())/** 主线程调度器传递消息给订阅者 */
+                .subscribe(this::doOnUIThread);/** 订阅者最终处理消息 */
+    }
+
+    public String doInBackground(Bitmap bitmap) {
+        try {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int[] pixels = new int[width * height];
+            bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+            RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
+            Result result = new MultiFormatReader().decode(new BinaryBitmap(new HybridBinarizer(source)), HINTS);
+            return result.getText();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void doOnUIThread(String result) {
+        Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            Toast.makeText(MainActivity.this, scanResult, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
