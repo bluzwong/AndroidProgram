0275ac383f22dc24a9a1539ca044b07fe4029770
bruce.home <766796371@qq.com>
2016-04-29 21:32:55 +0800

    bluz refactoring

--- Zxing_RxAndroid_Test/app/src/main/java/com/anonymous/myapplication/MainActivity.java
+++ Zxing_RxAndroid_Test/app/src/main/java/com/anonymous/myapplication/MainActivity.java
@@ -2,40 +2,36 @@ package com.anonymous.myapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Result;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import com.google.zxing.*;
 import com.google.zxing.client.android.decode.CaptureActivity;
 import com.google.zxing.client.android.encode.QRCodeEncoder;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.encoder.QRCode;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 import java.util.EnumMap;
 import java.util.Map;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 0;
 
-    @Bind(R.id.qrcode_encode) Button mEncodeBtn;
-    @Bind(R.id.qrcode_decode) Button mDecodeBtn;
-    @Bind(R.id.qrcode_img) ImageView mImageView;
+    @Bind(R.id.qrcode_encode)
+    Button mEncodeBtn;
+    @Bind(R.id.qrcode_decode)
+    Button mDecodeBtn;
+    @Bind(R.id.qrcode_img)
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +44,87 @@ public class MainActivity extends AppCompatActivity {
         mDecodeBtn.setOnClickListener(this::gotoCapture);
         // 长按解析二维码事件
         mImageView.setOnLongClickListener(this::decodeQrcode);
+
+
+        /// bluz version
+
+        mEncodeBtn.setOnClickListener(view ->
+                getQrBitmapByString("https://github.com/Res2013/AndroidProgram.git")
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(bitmap -> mImageView.setImageBitmap(bitmap),
+                                throwable -> throwable.printStackTrace())
+        );
+
+        mImageView.setOnClickListener(view ->
+                getBitmapByView(view)
+                        .flatMap(bitmap ->
+                                getStringByQrBitmap(bitmap).subscribeOn(Schedulers.computation()))
+                        .subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Subscriber<String>() {
+                            @Override
+                            public void onCompleted() {
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                e.printStackTrace();
+                            }
+
+                            @Override
+                            public void onNext(String result) {
+                                Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+                            }
+                        }));
+
+    }
+
+
+    public Observable<Bitmap> getBitmapByView(View view) {
+        return Observable.just(view)
+                .map(view1 -> {
+                    view.setDrawingCacheEnabled(true);
+                    Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+                    view.setDrawingCacheEnabled(false);
+                    return bitmap;
+                });
+    }
+
+    public Observable<String> getStringByQrBitmap(Bitmap bitmap) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                int[] pixels = new int[width * height];
+                bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+                RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
+                try {
+                    Result result = new MultiFormatReader().decode(new BinaryBitmap(new HybridBinarizer(source)), HINTS);
+                    if (!subscriber.isUnsubscribed()) {
+                        subscriber.onNext(result.getText());
+                    }
+                } catch (NotFoundException e) {
+                    subscriber.onError(e);
+                }
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    public Observable<Bitmap> getQrBitmapByString(String text) {
+        return Observable.create((Observable.OnSubscribe<Bitmap>) subscriber -> {
+            try {
+                Bitmap bitmap = QRCodeEncoder.encodeAsBitmap(text, 600);
+                if (!subscriber.isUnsubscribed()) {
+                    subscriber.onNext(bitmap);
+                }
+            } catch (WriterException e) {
+                subscriber.onError(e);
+            }
+            subscriber.onCompleted();
+        });
     }
 
     /**
@@ -68,8 +145,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-    * 解析二维码
-    */
+     * 解析二维码
+     */
     public boolean decodeQrcode(View v) {/** 看起来变量v有点多余,为啥要用?是因为参数和返回值同Lambda表达式一样,可用方法引用形式(即用方法名) */
         mImageView.setDrawingCacheEnabled(true);
         Bitmap bitmap = Bitmap.createBitmap(mImageView.getDrawingCache());
@@ -78,10 +155,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     /**
      * 要解析的二维码图片
      */
-    public final Map<DecodeHintType,Object> HINTS = new EnumMap<>(DecodeHintType.class);
+    public final Map<DecodeHintType, Object> HINTS = new EnumMap<>(DecodeHintType.class);
 
     public void decodeBitmap(Bitmap bitmap) {
         Observable.just(bitmap)/** just:获取输入数据,直接分发,更加简洁,省略其他回调 */
